dd602e3d1cc737494e338ef35e0b96db1700a0e8
LironSif <lironsifado@gmail.com>
2023-10-06 17:09:38 +0300

    new css

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const newGamePlayer = {
     current:0,
     score:0
 }
-
+// check with harosh and hadash conflict
 const players = {
     player1: {
         current:0,
@@ -37,14 +37,17 @@ let p1currentScore = document.querySelector ('.p1current')
 let p2currentScore = document.querySelector ('.p2current')
 let p1TotalScore = document.querySelector ('.p1Total')
 let p2TotalScore = document.querySelector ('.p2Total')
+let player1Div = document.querySelector('.player-1');
+let player2Div = document.querySelector('.player-2');
 
-// listeners...............................
+
+// listeners...............................................................................
 
 
-// ...................Smart Phone Rotate..........................................
+// ...................Smart-Phone-Rotate....................EventListener..................
 function checkOrientation() {
     let messageElement = document.getElementById('rotate-message');
-    
+
     if (window.matchMedia("(orientation: portrait)").matches && window.matchMedia("(max-width: 48em)").matches) {
         messageElement.style.display = 'block';
     } else {
@@ -56,21 +59,23 @@ window.addEventListener('resize', checkOrientation);
 
 
 
-// ...................starting..........................................
+// ...................Game-Start............................EventListener..................
 startBtn.addEventListener("click", function(){  
     target = targetNum.value
     startingDiv.style.display = 'none';
-    console.log(target)
+    PlayerHighlight()
 
 });
 
 
-// ...................intNewGame..........................................
-intNewGame.addEventListener("click", function(){  s});
+// ...................Init-Game............................EventListener..................
+intNewGame.addEventListener("click", function(){ 
+    location.reload();
+});
 
 
 
-// ...................rollTheDice..........................................
+// ...................roll-The-Dice........................EventListener..................
 rollTheDice.addEventListener("click", function(){ 
 
     const [rand1, rand2] = rnNum(); 
@@ -79,51 +84,70 @@ rollTheDice.addEventListener("click", function(){
     activePlayer.current += rand1 + rand2;
 
     if( players.activePlayer === 1){
-    p1currentScore.innerText = activePlayer.current
-    console.log(activePlayer.score)
+        if(rand1 + rand2 === 12) {
+        alert('Double six rolled!')
+        p1currentScore.innerText = 0;
+        players.activePlayer = 2
+        PlayerHighlight()
+        }else {
+        p1currentScore.innerText = activePlayer.current
+        }
     if(activePlayer.current + activePlayer.score > target) {
-    alert(`${activePlayer} loses`);
+        alert('Player 1 lost. Please start a new game.');
+
 
 
     }
     }else if( players.activePlayer === 2){
-    p2currentScore.innerText = activePlayer.current
+        if(rand1 + rand2 === 12) {
+        activePlayer.current = 0
+        alert('Double six rolled!')
+        p2currentScore.innerText = 0;
+        players.activePlayer = 1
+        PlayerHighlight()
+        }else {
+        p2currentScore.innerText = activePlayer.current
+         }
     if(activePlayer.current + activePlayer.score > target) {
-        // need to fix 
-    alert(`${activePlayer} loses`);
+      
+        alert('Player 2 lost. Please start a new game.');
+
 
     }
    }
   });
 
 
-// ...................changPlayer..........................................
+// ...................Change-Player........................EventListener..................
 changPlayer.addEventListener("click", function(){
 
     let activePlayer = players[`player${players.activePlayer}`];
     if(players.activePlayer === 1){
         activePlayer.score += activePlayer.current;
-        p1TotalScore.innerText = activePlayer.score
+        p1TotalScore.innerText = activePlayer.score;
         p1currentScore.innerText = 0;
+        players[`player${players.activePlayer}`].current =0
         players.activePlayer = 2
+        PlayerHighlight()
+
     } else if(players.activePlayer === 2){
         activePlayer.score = activePlayer.current;
-        p2TotalScore.innerText = activePlayer.score
+        p2TotalScore.innerText = activePlayer.score;
         p2currentScore.innerText = 0;
+        players[`player${players.activePlayer}`].current =0
         players.activePlayer = 1
+        PlayerHighlight()
     }
 });
 
 
 
-// functions...............................
-
-
-const newGame = () => {}
+// functions..............................................................................
 
 const activePlayer = () => {}
 
 
+// ...................Random-Number-Genrator...................Func.......................
 const rnNum = () => {
     rand1 = Math.ceil(Math.random()*6)
     rand2 = Math.ceil(Math.random()*6)
@@ -131,6 +155,7 @@ const rnNum = () => {
     }
 
 
+// ...................Image-Genrator...........................Func......................
 const changeImageSrc = (rand1, rand2) => {
     const diceNum1 = `/images/dice-${rand1}.png`;
     dice1.setAttribute("src", diceNum1);
@@ -138,7 +163,17 @@ const changeImageSrc = (rand1, rand2) => {
         dice2.setAttribute("src", diceNum2);
     }
 
-const curruntScore = () => {}
+// const curruntScore = () => {}
 
-// const activePlayer = () => {}
+const PlayerHighlight = () => {
+
+    if (players.activePlayer === 1) {
+        player1Div.classList.add('current-player');
+        player2Div.classList.remove('current-player');
+    } else {
+        player1Div.classList.remove('current-player');
+        player2Div.classList.add('current-player');
+    }
+}
+
 
